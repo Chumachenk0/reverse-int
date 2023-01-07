40edca2e4490d026d5fc749182e1604dc05f8711
Chumachenk0 <pavelchymachenko@gmail.com>
2023-01-07 23:57:26 +0200

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var n = n.toString().split("").reverse().join("");
+    var n = parseFloat(n);
+    return Math.abs(n);
+};
